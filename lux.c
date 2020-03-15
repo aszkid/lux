1be958eec45b8c5b02c2c9e25506e1f0886e92ca
Pol Gómez Riquelme <polgomezriquelme@gmail.com>
2020-03-15 14:58:33 -0500

    A red sphere and a green sphere

--- lux.c
+++ lux.c
@@ -9,19 +9,19 @@ typedef struct {
     double depth;
 } collision_t;
 
-typedef bool collide(vec3, void*, collision_t*);
+typedef bool collide(vec3, vec3, void*, collision_t*);
 
 typedef struct {
+    vec3 color;
     double r;
     vec3 pos;
 } sphere_t;
 
-bool test_ray_sphere(vec3 ray, void *obj, collision_t *col)
+bool test_ray_sphere(vec3 camera, vec3 ray, void *obj, collision_t *col)
 {
     sphere_t *s = (sphere_t*) obj;
-    vec3 p = {0.0, 0.0, -1.0};
     vec3 m;
-    vec3_sub(p, s->pos, &m);
+    vec3_sub(camera, s->pos, &m);
 
     double b = vec3_dot(m, ray);
     double c = vec3_dot(m, m) - s->r*s->r;
@@ -36,7 +36,12 @@ bool test_ray_sphere(vec3 ray, void *obj, collision_t *col)
 
     vec3 q = ray;
     vec3_mul(q, t, &q);
-    vec3_add(q, p, &q);
+    vec3_add(q, camera, &q);
+
+    col->color.x = s->color.x;
+    col->color.y = s->color.y;
+    col->color.z = s->color.z;
+
     return true;
 }
 
@@ -63,8 +68,8 @@ int render_job(ppm_t *ppm, void* data, collide *test, size_t obj_size, size_t ob
 
             // for each object given, test ray
             for (size_t k = 0; k < obj_n; k++) {
-                if (test(ray, data + k * obj_size, &col)) {
-                    ppm_write_at(ppm, i, j, 255, 0, 0);
+                if (test(camera, ray, data + k * obj_size, &col)) {
+                    ppm_write_at(ppm, i, j, 255.0 * col.color.x, 255.0 * col.color.y, 255.0 * col.color.z);
                 }
             }
         }
@@ -76,25 +81,6 @@ int render_job(ppm_t *ppm, void* data, collide *test, size_t obj_size, size_t ob
 
 int main(int argc, char **argv)
 {
-    vec3 a = {1., 5., 0.};
-    vec3 b = {3., -1., 0.};
-    printf("a = (%f, %f, %f)\n", VEC3_UNPACK(a));
-    printf("b = (%f, %f, %f)\n", VEC3_UNPACK(b));
-    printf("dot(a, b) = %f\n", vec3_dot(a, b));
-    vec3 c;
-    vec3_add(a, b, &c);
-    printf("add(a, b) = (%f, %f, %f)\n", VEC3_UNPACK(c));
-
-    vec3 i = {1., 0., 0.};
-    vec3 j = {0., 1., 0.};
-    vec3 k = {0., 0., 1.};
-    printf("i = (%f, %f, %f)\n", VEC3_UNPACK(i));
-    printf("j = (%f, %f, %f)\n", VEC3_UNPACK(j));
-    printf("k = (%f, %f, %f)\n", VEC3_UNPACK(k));
-    vec3 k_q;
-    vec3_cross(i, j, &k_q);
-    printf("cross(i, j) = (%f, %f, %f) = k? %s\n", VEC3_UNPACK(k_q), vec3_eq(k_q, k) ? "YES" : "NO");
-
     ppm_t *ppm = ppm_create("test.ppm", 1000, 1000);
 
     sphere_t spheres[2];
@@ -103,11 +89,17 @@ int main(int argc, char **argv)
     spheres[0].pos.x = -0.5;
     spheres[0].pos.y = 0.0;
     spheres[0].pos.z = 1.0;
+    spheres[0].color.x = 1.0;
+    spheres[0].color.y = 0.0;
+    spheres[0].color.z = 0.0;
     // sphere 1
     spheres[1].r = 0.5;
     spheres[1].pos.x = 0.5;
     spheres[1].pos.y = 0.0;
     spheres[1].pos.z = 1.0;
+    spheres[1].color.x = 0.0;
+    spheres[1].color.y = 1.0;
+    spheres[1].color.z = 0.0;
 
     render_job(ppm, spheres, &test_ray_sphere, sizeof(sphere_t), 2);
     ppm_close(ppm);
